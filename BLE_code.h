d283717f886fbffb23dcfbcf6d777113e5e1d4d8
emilnoraas <64132306+emilnoraas@users.noreply.github.com>
2020-04-23 17:38:29 +0200

    Create BLE_code.h

---
+++ BLE_code.h
@@ -0,0 +1,9 @@
+#ifndef INCLUSION_GUARD_BLE_H
+#define INCLUSION_GUARD_BLE_H
+
+void Ble_read(void);
+void BLE_Print(const char *fmt, ...);
+void Init(void);
+//int SPI_init(void);
+
+#endif // INCLUSION_GUARD_BLE_H
